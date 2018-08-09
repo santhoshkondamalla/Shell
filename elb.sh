37d57a6378a464eea64cd14045f922c0e0f8d5f4
santhoshkondamalla <kondamallasanthosh@gmail.com>
2018-08-09 17:27:47 +0530

    Update elb.sh

--- elb.sh
+++ elb.sh
@@ -4,11 +4,11 @@ status=`aws --profile my-ap-south-1 elb describe-load-balancers --output=text | 
 for i in $status
 do
 status1=`aws --profile my-ap-south-1 elb describe-instance-health --load-balancer-name $i|grep -o OutOfService`
-if [ "$status1" == "OutOfService" ]
+nodes=$(aws elb describe-instance-health --load-balancer-name $i  --output text  --query 'InstanceStates[*].[InstanceId,State]'  --profile my-ap-south-1  | grep -i outofservice)
+
+if [ "$status1" =  "OutOfService"  ]
 then
-echo "The failed instances are given below ELB $i"
-#aws  --profile my-ap-south-1 elb describe-instance-health --load-balancer-name $i | grep -w "i*"
-aws elb describe-instance-health --load-balancer-name $i  --output text  --query 'InstanceStates[*].{InstanceID: InstanceId,StateName: State}'  --profile my-ap-south-1  | grep -i outofservice
+echo "The failed instances are given below ELB $i nodes are $nodes"
 else
 echo "No instances failed"
 fi
